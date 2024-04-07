358d4d48c2680e61044c62c4046331f2b957209b
Claudia Zarzuela <100291375+ClaudiaZarzuela@users.noreply.github.com>
2024-04-07 17:45:25 +0200

    meshes

--- src/CrossthePath/CTPMain.cpp
+++ src/CrossthePath/CTPMain.cpp
@@ -1,5 +1,13 @@
 #include "CTPMain.h"
+#include "ScriptManager.cpp"
 
 std::string saludo() {
 	return "adios";
+}
+
+void loadScene() {
+	eden_script::ScriptManager* scriptMngr = eden_script::ScriptManager::Instance();
+	//scriptMngr->ReadScene("Menu",)
+	/*eden::SceneManager* scnManager = eden::SceneManager::Instance();
+	scnManager->PushScene("Menu");*/
 }
--- src/CrossthePath/CTPMain.h
+++ src/CrossthePath/CTPMain.h
@@ -2,6 +2,10 @@
 
 #include <string>
 
+namespace eden_script {
+	class ScriptManager;
+}
 extern "C" __declspec(dllexport) std::string saludo();
 
+extern "C" __declspec(dllexport) void loadScene();
 #endif // !CROSS_THE_PATH_MAIN_H_
