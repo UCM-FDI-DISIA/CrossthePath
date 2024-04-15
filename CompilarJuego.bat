8b8e06d9062fdea3b401a864ca02dd639c72d140
almasso <almasso@ucm.es>
2024-04-15 20:03:08 +0200

    Bats de compilación del juego y actualización de EDEN
    NOTA: No subir los cambios sobre el submódulo EDEN cuando se haga un commit

---
+++ CompilarJuego.bat
@@ -0,0 +1,12 @@
+@echo off
+
+call ActualizarMotor.bat
+
+set GAMEDLLFOLDERS=bin
+
+for %%i in (".\bin\debug\*") do (
+        XCOPY /y "%%i" %GAMEDLLFOLDERS%
+)
+
+msbuild "CrossthePath.sln" /p:configuration=Debug /p:Platform=x64
+msbuild "CrossthePath.sln" /p:configuration=Release /p:Platform x64
