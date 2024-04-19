258d9fda43e3ebe96ae4f08378ef4fba96945563
Andrea Vega Saugar <anvega03@ucm.es>
2024-04-19 10:32:48 +0200

    Comienzo instanciateEnemies Component

---
+++ projects/CrossthePath/InstanciateEnemies.h
@@ -0,0 +1,18 @@
+#ifndef CROSS_THE_PATH_C_INSTANCIATE_ENEMIES_H
+#define CROSS_THE_PATH_C_INSTANCIATE_ENEMIES_H
+
+#include "Component.h"
+
+namespace eden {
+	class SceneManager;
+}
+
+namespace eden_ec {
+	class CTransform;
+	class InstanciateEnemies : public Component
+	{
+
+	};
+}
+
+#endif // CROSS_THE_PATH_C_INSTANCIATE_ENEMIES_H
