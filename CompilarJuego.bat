ac57e4a89a1a16d3d4ca1a77ba3f1f2c37912e8e
almasso <almasso@ucm.es>
2024-05-10 03:06:28 +0200

    Bat para la escena inicial

--- CompilarJuego.bat
+++ CompilarJuego.bat
@@ -3,10 +3,14 @@
 call ActualizarMotor.bat
 
 set GAMEDLLFOLDERS=.\bin
+set sourceFolder=.\EDEN\bin\assets
+set destinationFolder=.\bin\assets
 
 for %%i in (".\EDEN\bin\*") do (
         XCOPY /y "%%i" %GAMEDLLFOLDERS%
 )
+
+xcopy "%sourceFolder%\*" "%destinationFolder%\" /E /Y
 
 msbuild "CrossthePath.sln" /p:configuration=Debug /p:Platform=x64
 msbuild "CrossthePath.sln" /p:configuration=Release /p:Platform=x64
